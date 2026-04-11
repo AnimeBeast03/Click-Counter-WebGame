ae6ce24f7743db071d3b32e1a530e3d2bff89f2f
Prathamesh Bhembre <prathameshprabhubhembre@gmail.com>
2026-04-11 21:21:33 +0530

    Increased maximum clickability to 200 clicks and given a popup effect to the counter.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var lastClick = 0;
 var freshClick = 0;
 var clickTime = 200; // miliseconds
 var count = 0;
-var maxCount = 20;
+var maxCount = 200;
 var colorChangeAt = 10;
 
 // starting game state
@@ -18,45 +18,53 @@ resetBtn.style.display = "none";
 msg.style.display = "none";
 
 // when click button is pressed
-clickBtn.addEventListener("click",function() {
-  if (count < maxCount) {
-    checkClickTime();
-    update();
-  } else {
-    msg.style.display = "block";
-    msg.textContent = "Limit Reached";
-  }
+clickBtn.addEventListener("pointerdown",function() {
+    if (count < maxCount) {
+        // give a popup effect to the counter
+        counter.animate([
+            { fontSize: "50vw", top: "5vh" },
+            { fontSize: "60vw", top: "0vh" },
+            { fontSize: "50vw", top: "5vh" }
+        ], {
+            duration: 250
+        });
+        checkClickTime();
+        update();
+    } else {
+        msg.style.display = "block";
+        msg.textContent = "Limit Reached";
+    }
 });
+
 //when reset button is pressed
-resetBtn.addEventListener("click",function() {
-  count = 0;
-  counter.textContent = count;
-  resetBtn.style.display = "none";
-  msg.style.display = "none";
+resetBtn.addEventListener("pointerdown",function() {
+    count = 0;
+    counter.textContent = count;
+    resetBtn.style.display = "none";
+    msg.style.display = "none";
 });
 
-// check time between two clicks and shiw fire
+// check time between two clicks and show fire emoji
 function checkClickTime() {
-  freshClick = Date.now();
-  if (freshClick - lastClick < clickTime) {
-    msg.style.display = "block";
-    msg.textContent = String.fromCodePoint(0x1F525);
-  } else {
-    msg.style.display = "none";
-  }
-  lastClick = freshClick;
+    freshClick = Date.now();
+    if (freshClick - lastClick < clickTime) {
+        msg.style.display = "block";
+        msg.textContent = String.fromCodePoint(0x1F525);
+    } else {
+        msg.style.display = "none";
+    }
+    lastClick = freshClick;
 }
-// update count and change color after every 10th count
+
+// update counter and change text color every on 10th count
 function update() {
-  count+=1;
-  counter.textContent = count;
-  resetBtn.style.display = "block";
-  if (Number.isInteger(count/colorChangeAt)) {
-    var r = Math.floor(Math.random()*226);
-    var g = Math.floor(Math.random()*226);
-    var b = Math.floor(Math.random()*226);
-    counter.style.color = "rgb(" + r + "," + g + "," + b + ")";
-  }
+    count+=1;
+    counter.textContent = count;
+    resetBtn.style.display = "block";
+    if (Number.isInteger(count/colorChangeAt)) {
+        var r = Math.floor(Math.random()*226);
+        var g = Math.floor(Math.random()*226);
+        var b = Math.floor(Math.random()*226);
+        counter.style.color = "rgb(" + r + "," + g + "," + b + ")";
+    }
 }
-
-
